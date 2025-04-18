4a89528979d4b02094c81bc15a241fe79793c998
Rai Zela <raizela85@gmail.com>
2025-04-18 11:19:42 +0200

    work in progress journey application layer

--- src/Services/JourneyService/Journey.Application/GlobalUsing.cs
+++ src/Services/JourneyService/Journey.Application/GlobalUsing.cs
@@ -1,8 +1,10 @@
-﻿global using Microsoft.EntityFrameworkCore;
-global using JourneyEntity = Journey.Domain.Models.Journey;
-global using BuildingBlocks.CQRS;
-global using Journey.Application.Data;
-global using Journey.Application.Journeys.Commands.CreateOrder;
+﻿global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions;
+global using Journey.Application.Data;
 global using Journey.Application.Dtos;
 global using Journey.Application.Exceptions;
+global using Journey.Application.Journeys.Commands.CreateOrder;
+global using Journey.Domain.Events;
+global using MediatR;
+global using Microsoft.EntityFrameworkCore;
+global using JourneyEntity = Journey.Domain.Models.Journey;
---
+++ src/Services/JourneyService/Journey.Application/Journeys/EventHandlers/JourneyCreatedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Journey.Application.Journeys.EventHandlers;
+
+public class JourneyCreatedEventHandler
+    //(ILogger<JourneyCreatedEventHandler> logger)
+    : INotificationHandler<JourneyCreatedEvent>
+{
+    public Task Handle(JourneyCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        //logger.LogInformation("Domain Event Handled: {DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
+
