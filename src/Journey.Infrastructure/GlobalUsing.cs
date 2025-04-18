95f04584441a6237b3926b6af3c51bd9643f8cdc
Rai Zela <raizela85@gmail.com>
2025-04-18 17:23:30 +0200

    globalusing refactoring

--- src/Journey.Infrastructure/GlobalUsing.cs
+++ src/Journey.Infrastructure/GlobalUsing.cs
@@ -2,11 +2,13 @@
 global using Journey.Domain.Abstractions;
 global using Journey.Domain.Models;
 global using Journey.Domain.ValueObjects;
+global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.ChangeTracking;
 global using Microsoft.EntityFrameworkCore.Diagnostics;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.DependencyInjection;
 global using System.Reflection;
 global using System.Security.Claims;
 global using JourneyEntity = Journey.Domain.Models.Journey;
