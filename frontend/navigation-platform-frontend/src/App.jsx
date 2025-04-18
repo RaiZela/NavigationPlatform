5c098103a212a4b39ab00900f3de3c46e5a7200c
Rai Zela <raizela85@gmail.com>
2025-04-19 01:28:13 +0200

    work in progress front end

---
+++ frontend/navigation-platform-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AuthProvider } from './context/AuthContext';
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <AuthProvider>
+      <div className="App">
+        <Login />
+      </div>
+    </AuthProvider>
+  );
+}
+
+
+export default App;
+
