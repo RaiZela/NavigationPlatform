684a59917e9face9e101957e6ce7670b97578dfb
Rai Zela <raizela85@gmail.com>
2025-07-23 16:48:20 +0200

    journey filter

---
+++ src/BuildingBlocks/BuildingBlocks/Filtering/JourneyFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace BuildingBlocks.Filtering;
+
+public class JourneyFilter
+{
+    public Guid UserId { get; set; } = Guid.Empty;
+    public TransportType TransportType { get; set; } = TransportType.None;
+    public DateTime? StartDateFrom { get; set; }
+    public DateTime? StartDateTo { get; set; }
+    public DateTime? ArrivalDateFrom { get; set; }
+    public DateTime? ArrivalDateTo { get; set; }
+    public decimal MinDistance { get; set; }
+    public decimal MaxDistance { get; set; }
+}
